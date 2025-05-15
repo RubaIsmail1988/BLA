3484e2abb69d4c445457465b74c982e02c44e5b8
MohamadAlras <alrasmoh.80@gmail.com>
2025-05-16 01:29:30 +0300

    modify settings for deployment

--- BLA/settings.py
+++ BLA/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)^cs1d^hf$^21@p6aku(mg39uk6v8))&f2_y5h@9vvz%p&56gz'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['*']
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
 
 
 # Application definition
