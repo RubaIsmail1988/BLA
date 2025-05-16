ac75ec06aa05bf23df12d557a82234ed02d271ff
MohamadAlras <alrasmoh.80@gmail.com>
2025-05-17 00:08:53 +0300

    modify wsgi for deployment

--- BLA/wsgi.py
+++ BLA/wsgi.py
@@ -6,11 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
+
+path = '/home/mohamadalras80/BLA/BLA'  
+if path not in sys.path:
+    sys.path.insert(0, path)  
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BLA.settings')
+
 
 application = get_wsgi_application()
